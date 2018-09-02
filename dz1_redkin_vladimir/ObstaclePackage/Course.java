fc245dcad39cda94ee00800133675d14939d9af1
vic <ab9171818@gmail.com>
2018-09-02 13:03:58 -0500

    delete

--- dz1_redkin_vladimir/ObstaclePackage/Course.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.java2.dz.dz1_redkin_vladimir.ObstaclePackage;
-
-import ru.geekbrains.java2.dz.dz1_redkin_vladimir.ParticipantPackage.Team;
-
-public interface Course {
-    boolean doIt(Team t);
-}
